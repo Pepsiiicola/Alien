a9b05c22a5e7e8ab8c4d3500b8bbf42d0a895102
shenwc <1415538275@qq.com>
2025-06-27 13:08:54 +0800

    modified all files

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,28 +9,54 @@ class AlienInvasion():
         pygame.init()
         self.settings = Settings()
         # 创建一个显示窗口，大小为1200*800
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        # 创建一个ship实例
         self.ship = Ship(self)
 
     def run_game(self):
 
+
+        """开始游戏的主循环"""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            
+
+
+    def _check_events(self):
+        """相应鼠标和键盘时间"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # 向右移动飞船
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # 向左移动飞船
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+    
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
         # 每次循环时都重绘屏幕
         self.screen.fill(self.settings.bg_color)
+
+        # 调用ship.blitme()将飞船绘制到屏幕上
+        self.ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
-        """开始游戏的主循环"""
-        while True:
-            # 监听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+
             
-            
-        
     
 if __name__ == "__main__":
     # 创建游戏实例并运行游戏
--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-class Settings(object):
+class Settings():
     """存储游戏《外星人入侵》中所有设置的类"""
     def __init__(self):
         """初始化游戏的设置"""
@@ -6,3 +6,6 @@ class Settings(object):
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230,230,230)
+
+        # 飞船设置
+        self.ship_speed = 1.5
